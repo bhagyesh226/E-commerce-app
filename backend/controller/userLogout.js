f6e4aedefeaa26da0cc6595a55d50cee7d1e2c57
bhagyesh226 <bhagyeshk226@gmail.com>
2025-06-19 12:42:34 +0530

    first commit 4

--- backend/controller/userLogout.js
+++ backend/controller/userLogout.js
@@ -1,20 +1,24 @@
-async function userLogout (req,res){
-    try{
-res.clearCookie("token")
+async function userLogout(req, res) {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'None'
+    });
 
-res.json({
-    message:"user Logout",
-    error:false,
-    success:true,
-    data:[]
-})
-    }catch(err){
-         res.status(400).json({
-            message : err.message || err,
-            error: true,
-            succss: false
-        })
-    }
+    res.json({
+      message: "User logged out",
+      error: false,
+      success: true,
+      data: []
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: err.message || err,
+      error: true,
+      success: false
+    });
+  }
 }
 
-module.exports = userLogout
+module.exports = userLogout;
